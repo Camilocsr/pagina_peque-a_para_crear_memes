413a576fd3cde631c5a83e29d139bbeea98070f6
Camilocsr <esmunred@gmail.com>
2022-11-04 10:57:41 -0500

    pagina de memes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import html2canvas from 'html2canvas';
 
 function App() {
+  const [linea1,setlinea1] = useState('');
+  const [linea2,setlinea2] = useState('');
+  const [img,setImagen] = useState('');
+
+  const onChangeLinea1 = function(valor) {
+    setlinea1(valor.target.value);
+  }
+
+  const onChangeLinea2 = function(valor) {
+    setlinea2(valor.target.value);
+  }
+
+  const onChangeimagen = function(valor) {
+    setImagen(valor.target.value);
+  }
+
+  const onClickExportar = function() {
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      let img = canvas.toDataURL("image/png");
+      let link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='meme'>
+      <select onChange={onChangeimagen}>
+        <option value="fire">casa en llamas</option>
+        <option value="futurama">futurama</option>
+        <option value="history">History chanel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">philosofoRaptor</option>
+        <option value="smart">Smath guy</option>
+      </select><br/>
+        <input onChange={onChangeLinea1} placeholder="linea 1" type="text"/><br/>
+        <input onChange={onChangeLinea2} placeholder="linea 2" type="text"/><br/>
+        <button onClick={onClickExportar}>exportar</button>
+
+        <div className='meme'>
+          <span>{linea1}</span> <br/>
+          <span>{linea2}</span>
+          <img src={"/images/"+img+".png"} width="100%"/>
+        </div>
+
     </div>
   );
 }
